121c34c5b49711c5ddd4d029083131045c72f508
Koma07 <ariasluis2406@gmail.com>
2023-05-16 21:50:50 -0400

    Added Day 8

---
+++ Day 8/js/script.js
@@ -0,0 +1,30 @@
+const emailInput = document.getElementById("email-form");
+const passwordInput = document.getElementById("password-form");
+const emailLabel = document.getElementById("email-label");
+const passwordLabel = document.getElementById("password-label");
+
+// Function to see which input is selected, and changing the style of its correspondent label.
+const openText = () => {
+  // This is the condition to see if the email input is selected
+  if (document.activeElement === emailInput) {
+    emailLabel.classList.add("label-text-open");
+  } else {
+    emailLabel.classList.remove("label-text-open");
+  }
+  // This is the condition to see if the password input is selected
+  if (document.activeElement === passwordInput) {
+    passwordLabel.classList.add("label-text-open");
+  } else {
+    passwordLabel.classList.remove("label-text-open");
+  }
+};
+
+// Event listener to check constanly if the inputs are focused or not, using click and the blur events.
+
+// The blur event it's started when an input has lost its focus.
+emailInput.addEventListener("click", openText);
+emailInput.addEventListener("blur", openText);
+passwordInput.addEventListener("click", openText);
+passwordInput.addEventListener("blur", openText);
+
+console.log(emailInput.focus());
