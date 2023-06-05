a871b6ffef3bcee2035441f62d6ec7e14e5b98d1
Koma07 <ariasluis2406@gmail.com>
2023-06-04 20:36:44 -0400

    Changes on the comments for Day 16.

--- Day 16/js/script.js
+++ Day 16/js/script.js
@@ -10,29 +10,29 @@ let valueDrank = 12.5;
 let valueRemained = 87.5;
 let litersValue = 1.75;
 
-//Loop through the node of elements with the class of .glass.
+//Loop through the node of elements glassesNode and asigns the name glass for each one.
 glassesNode.forEach((glass) => {
-  // Added event listener to all the buttons with the class of .glass
+  // Adds an event listener to all the buttons inside the glassesNode.
   glass.addEventListener("click", () => {
     // Every time a button is clicked is gonna do this function
     if (glass.classList.contains("active")) {
       // Check if the button has the class of .active. If it is the case, is gonna substract 12.5 of the intial value of fill water (blue square height) and added to the remained value (white square height).
 
-      valueDrank -= 12.5; //Value used to substract 12.5 to the initial value
+      valueDrank -= 12.5; //Value used to substract 12.5 to the height of the water drank.
 
-      valueRemained += 12.5; // Value used to add 12.5 to the initial value
+      valueRemained += 12.5; // Value used to add 12.5 to height of the liters remained.
 
-      litersValue += 0.25;
+      litersValue += 0.25; // Value used add 0.25 to the liters value.
 
-      waterDrank.style.height = `${valueDrank}%`; //Substracts the value to the height of percentage water drank.
+      waterDrank.style.height = `${valueDrank}%`; //Asigns the value previously substracted to the height of percentage water drank.
 
-      waterNotDrank.style.height = `${valueRemained}%`; // Adds the value to the height of the percentage of water not drank.
+      waterNotDrank.style.height = `${valueRemained}%`; // Asigns the value previously added to the height of the percentage of water not drank.
 
-      waterLiters.innerHTML = `${litersValue}L`; // Adds the value of liters inside of the element to show how many liters remains to be drank
+      waterLiters.innerHTML = `${litersValue}L`; // Asigns the value of liters inside of the element to show how many liters remains to be drank
 
-      waterPercentage.innerHTML = `${valueDrank}%`; // Adds the initial value inside of the element to show the percentage of water drank
+      waterPercentage.innerHTML = `${valueDrank}%`; // Asings the value previously added inside of the element to show the percentage of water drank
 
-      // Checks if the value is zero to not display the element where is show the percentage to be drank
+      // Checks if the value of water drank is zero to not display the element where is show the percentage to be drank
       if (valueDrank == 0) {
         waterPercentage.style.display = "none";
       } else {
@@ -41,21 +41,21 @@ glassesNode.forEach((glass) => {
 
       glass.classList.toggle("active"); // Adds or removes the class active to the button clicked. That changes styles on the button.
     } else {
-      // If the button has not the class of .active is gonna add 12.5 to the height of fill water (blue square) and is gonna substract 12.5 to the remained value (87.5) (white square).
+      // If the button has not the class of .active is gonna add 12.5 to the height of water drank (blue square) and is gonna substract 12.5 to the liters remained (white square).
 
-      valueDrank += 12.5; // Value used to add 12.5 to the initial value.
+      valueDrank += 12.5; // Value used to add 12.5 to the water drank.
 
-      valueRemained -= 12.5; //Value used to substract 12.5 to the initial value,
+      valueRemained -= 12.5; // Value used to substract 12.5 to liters remained,
 
-      litersValue -= 0.25;
+      litersValue -= 0.25; // Value used substract 0.25 to the liters value.
 
-      waterDrank.style.height = `${valueDrank}%`; // Adds the value to the height of the percentage of water  drank.
+      waterDrank.style.height = `${valueDrank}%`; //Asigns the value previously added to the height of percentage water drank.
 
-      waterNotDrank.style.height = `${valueRemained}%`; //Substracts the value to the height of percentage water not drank.
+      waterNotDrank.style.height = `${valueRemained}%`; // Asigns the value previously substracted to the height of the percentage of water not drank.
 
-      waterLiters.innerHTML = `${litersValue}L`; // Adds the value of liters inside of the element to show how many liters remains to be drank
+      waterLiters.innerHTML = `${litersValue}L`; // Asigns the value of liters inside of the element to show how many liters remains to be drank
 
-      waterPercentage.innerHTML = `${valueDrank}%`; // Adds the initial value inside of the element to show the percentage of water drank
+      waterPercentage.innerHTML = `${valueDrank}%`; // Asings the value previously added inside of the element to show the percentage of water drank
 
       // Checks if the value is zero to not display the element where is show the percentage to be drank
       if (valueDrank == 0) {
